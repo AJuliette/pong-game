c5be7deec9f6f1cd720de53889aa0776b6992d29
Juliette Audema <juliette.audema@gmail.com>
2019-07-02 20:35:38 +0200

    Finish game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var startBtn = document.getElementById("startBtn");
+var restartBtn = document.getElementById("restart");
 
 startBtn.onclick = function() {
   startBtn.style.display = "none"
@@ -16,6 +17,10 @@ function Ball() {
   this.x = canvas.width / 2;
   this.y = canvas.height / 2;
   this.ballRadius = 10;
+
+  // responsible for the speed and direction
+  this.dy = 5;
+  this.dx = 5;
 }
 
 Ball.prototype.drawBall = function() {
@@ -23,6 +28,30 @@ Ball.prototype.drawBall = function() {
   ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2)
   ctx.fill()
 }
+
+Ball.prototype.checkBorder = function () {
+  if (
+    this.y + ball.dy > canvas.height - this.ballRadius
+    ||
+    // to test once the paddles are moving
+    this.y + this.dy < ball.ballRadius
+    ) {
+    this.dy = -this.dy
+  }
+}
+
+Ball.prototype.checkLeftAndRight = function () {
+  if (
+    ball.x > canvas.width + 2 * ball.ballRadius
+
+    ||
+    ball.x < -2 * ball.ballRadius
+  ) {
+    return true
+  }
+  return false
+}
+
 
 // ----------- PADDLE -----------
 function Paddle(x) {
@@ -38,11 +67,54 @@ Paddle.prototype.drawPaddle = function() {
   ctx.fillRect(this.x, this.y, this.width, this.height)
 }
 
+Paddle.prototype.hitPaddleTwo = function () {
+  if (
+    ball.x < paddle2.x + paddle2.width
+    &&
+    ball.x + ball.dx > paddle2.x
+    &&
+    ball.y < paddle2.y + paddle2.height
+    &&
+    ball.y + ball.dy > paddle2.y
+  ) {
+    ball.dx =- ball.dx
+  }
+}
+
+Paddle.prototype.hitPaddleOne = function () {
+  if (
+    ball.x < paddle1.x + paddle1.width
+    &&
+    ball.x + ball.dx > paddle1.x
+    &&
+    ball.y < paddle1.y + paddle1.height
+    &&
+    ball.y + ball.dy > paddle1.y
+  ) {
+    ball.dx =- ball.dx
+  }
+}
+
 function draw() {
+  // clear the canvas on each frame
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  // movement of ball  
+  ball.drawBall()
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  ball.checkBorder()
+  if (ball.checkLeftAndRight() == true) {
+    restartBtn.style.display = "inline"
+  }
+
   paddle1.drawPaddle()
   paddle2.drawPaddle()
-  ball.drawBall()
+
+  paddle2.hitPaddleTwo()
+  paddle1.hitPaddleOne()
+
   window.requestAnimationFrame(draw)
 }
 
