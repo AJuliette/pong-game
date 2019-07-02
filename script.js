6cc4f4ac010c63db6488a36af61c3de83b64163f
Juliette Audema <juliette.audema@gmail.com>
2019-07-02 18:57:06 +0200

    Fix second paddle and add ball

--- script.js
+++ script.js
@@ -6,9 +6,25 @@ startBtn.onclick = function() {
   startBtn.style.display = "none"
   paddle1 = new Paddle(10)
   paddle2 = new Paddle(970)
+  ball = new Ball()
   window.requestAnimationFrame(draw)
 }
 
+// ----------- BALL -----------
+function Ball() {
+  this.color = "tomato";
+  this.x = canvas.width / 2;
+  this.y = canvas.height / 2;
+  this.ballRadius = 10;
+}
+
+Ball.prototype.drawBall = function() {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2)
+  ctx.fill()
+}
+
+// ----------- PADDLE -----------
 function Paddle(x) {
   this.color = "tomato";
   this.x = x;
@@ -26,6 +42,7 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   paddle1.drawPaddle()
   paddle2.drawPaddle()
+  ball.drawBall()
   window.requestAnimationFrame(draw)
 }
 
@@ -34,6 +51,10 @@ function keyDownHandler(event) {
     paddle1.y -= 5 
   } else if (event.key === "s") {
     paddle1.y += 5
+  } else if (event.key === "ArrowUp") {
+  paddle2.y -= 5
+  } else if (event.key === "ArrowDown") {
+    paddle2.y += 5
   }
 }
 
